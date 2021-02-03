4929d917c9f98a15bce280e52d65d74e2ee5feff
badenb <badenb@email.uscb.edu>
2021-02-03 06:47:33 -0500

    gem initail commit

---
+++ lib/bennett_view_tool.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require_relative "bennett_view_tool/version"
+require_relative "bennett_view_tool/renderer"
+
+module BennettViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
